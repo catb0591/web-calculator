70f6c19402a10d2600849722be9132f06e30543d
Cat <catb0591@gmail.com>
2021-10-13 18:30:44 -0400

    Submission of Part One HW

---
+++ script.js
@@ -0,0 +1,6 @@
+function calculate() {
+    const firstValue = document.getElementById('First Value').value
+    const secondValue = document.getElementById('Second Value').value
+    const answer = (Number(firstValue) + Number(secondValue))
+    alert(answer)
+}
